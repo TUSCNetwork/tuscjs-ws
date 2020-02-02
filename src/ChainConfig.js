0131deb3c1cdfbdf4d6f7e7f1952c085526cc598
RarelyCharles <cwbwork@gmail.com>
2020-02-02 15:08:19 -0500

    Remove unnecessary chain ids.

--- src/ChainConfig.js
+++ src/ChainConfig.js
@@ -11,24 +11,12 @@ var config = {
       chain_id:
         "5a64159ecf7214e8d072712ed9341cda01d9a8d9f8d6ac554decb41542497168"
     },
-    Muse: {
-      core_asset: "MUSE",
-      address_prefix: "MUSE",
-      chain_id:
-        "45ad2d3f9ef92a49b55c2227eb06123f613bb35dd08bd876f2aea21925a67a67"
-    },
     Test: {
       core_asset: "TUSC",
       address_prefix: "TUSC",
       chain_id:
         "2f3b0eda49110b33e8992da47c73f96db7b8cf28e68d5dabf7e39e48a69371ed"
     },
-    Obelisk: {
-      core_asset: "GOV",
-      address_prefix: "FEW",
-      chain_id:
-        "1cfde7c388b9e8ac06462d68aadbd966b58f88797637d9af805b4560b0e9661e"
-    }
   },
 
   /** Set a few properties for known chain IDs. */
